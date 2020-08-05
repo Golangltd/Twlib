0241baaa7c156bae3aaab6cce53d607087e3e22f
byteedu <1528884535@qq.com>
2020-08-05 09:56:34 +0800

    server proto

---
+++ proto/proto.go
@@ -0,0 +1,23 @@
+package proto
+
+// 主协议 == 规则
+const (
+	INIT             = iota //  INIT == 0
+	GameDataProto           //  GameDataProto == 1      游戏代理的主协议       proxy server 协议
+	GameDataDBProto         //  GameDataDBProto == 2    游戏的DB的主协议       db server 协议
+	GameNetProto            //  GameNetProto == 3       游戏的NET主协议        心跳等
+	GErrorProto             //  GErrorProto == 4        游戏的错误处理         统一错误处理
+	GGateWayProto           //  GGateWayProto == 5      网关协议              暂时不用
+	GGameHallProto          //  GGameHallProto == 6     游戏主场景协议         game server 协议
+	GGameLoginProto         //  GGameLoginProto == 7    登录服务器协议         登录服务器主协议
+	GGameGMProto            //  GGameGMProto == 8       游戏GM管理系统
+	GGamePayProto           //  GGamePayProto == 9      游戏支付系统
+	GGameBattleProto        //  GGameBattleProto  == 10   游戏战斗系统
+)
+
+type GameProto64 struct {
+	Protocol  int
+	Protocol2 int
+	ProtoVer  string // 协议版本
+}
+
---
+++ server/server.go
@@ -0,0 +1,17 @@
+package server
+
+// 代理服务器地址
+const ProxyIp string = "127.0.0.1:8888"
+
+// 所有的内部服务器的ServerId
+const (
+	SERVER = iota
+	ProxyServerId   // ProxyServerId == 1
+	GameServerId    // GameServerId   == 2
+	BattleServerId  // BattleServerId == 3
+	GMServerId      // GMServerId == 4
+	DBServerId      // DBServerId == 5
+	CenterServerId  // CenterServerId == 6
+)
+
+
