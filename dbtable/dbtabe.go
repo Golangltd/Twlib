042ab2efa995400bcb0b898ce8aec8560ae91535
byteedu <1528884535@qq.com>
2020-08-06 17:22:09 +0800

    add userst

--- dbtable/dbtabe.go
+++ dbtable/dbtabe.go
@@ -2,5 +2,16 @@ package dbtable
 
 // 数据库对应的表名
 const (
-     RoleTable = "gl_js_jsb"   // 角色表
+	RoleTable = "gl_js_jsb"   // 角色表
 	)
+
+// 获取对应宏对应的表名
+func GetDbTableEnum(table string)  string{
+	switch table {
+	case RoleTable :
+      return RoleTable
+	default:
+		return ""
+	}
+	return ""
+}
