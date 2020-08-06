5bb35867e39a28debaa10ddcd4725caf1c56d7c2
byteedu <1528884535@qq.com>
2020-08-06 16:16:55 +0800

    add userst

--- cfgame/cfgame.go
+++ cfgame/cfgame.go
@@ -2,6 +2,12 @@ package cfgame
 
 // 获取配置数据库的协议
 const (
-	CFInit = iota
-	GetCFGameData    //GetCFGameData == 1 获取配置数据库数据
+	CFInit              = iota
+	GetCFGameDataProto2 // GetCFGameDataProto2 == 1 获取配置数据库数据
 )
+
+type GetCFGameData struct {
+	Protocol  int
+	Protocol2 int
+	TableName string // 表名
+}
