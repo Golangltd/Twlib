fa6728c6572ba7238b9e1f50f8f38e0997403ce1
byteedu <1528884535@qq.com>
2020-08-07 13:50:04 +0800

    add userst

--- cfgame/cfgame.go
+++ cfgame/cfgame.go
@@ -4,6 +4,7 @@ package cfgame
 const (
 	CFInit              = iota
 	GetCFGameDataProto2 // GetCFGameDataProto2 == 1 获取配置数据库数据
+	CFRetGameDataProto2 // CFRetGameDataProto2 == 2 返回的数据
 )
 
 type GetCFGameData struct {
@@ -11,4 +12,10 @@ type GetCFGameData struct {
 	Protocol2 int
 	TableName string // 表名
 	ServerId  string // 服务器Id
+}
+
+type CFRetGameData struct {
+	Protocol  int
+	Protocol2 int
+	Data      interface{}
 }
