18a60cfec80057b78a758cf58afb93096f5cb8ef
byteedu <1528884535@qq.com>
2020-09-05 10:08:44 +0800

    111

--- Chapter/Chapter.go
+++ Chapter/Chapter.go
@@ -9,6 +9,20 @@ const (
 
 // 章节相关
 type ChapterSt struct {
-	ChapterId    int // 章节Id
-	ChapterState int // 章节状态，是否解锁等
+	ChapterId    int              // 章节Id
+	ChapterState int              // 章节状态，是否解锁等
+	PlayerData   []*ChapterUserSt // 章节的用户信息
+}
+
+// 玩家结构
+type ChapterUserSt struct {
+	RoleUid    int64
+	RoleName   string
+	RoleAvatar int
+	RoleLev    int
+	RoleSex    int
+	RoleExp    int   // 巫师经验
+	Coin       int64 // 金币
+	Diamond    int64 // 砖石
+	ChannelId  int   // 渠道Id
 }
