e5719a37808a5e84894a6ff7fa125605972a221e
wanggang <418566859@qq.com>
2020-09-12 11:41:39 +0800

    modify event

--- event/event.go
+++ event/event.go
@@ -1,5 +1,18 @@
 package twlib_event
 
+// 活动条件类型
+const (
+	ConditionTypeRechargeSingle   = 10000 // 单笔充值金额
+	ConditionTypeRechargeSum      = 10002 // 累计充值金额
+	ConditionTypeRechargeSpecify  = 10003 // 指定充值ID
+	ConditionTypeLevelUser        = 10004 // 玩家等级
+	ConditionTypeLevelVip         = 10005 // VIP等级
+	ConditionTypeLoginContinueDay = 10006 // 连续登录天数
+	ConditionTypeLoginSumDay      = 10007 // 累计登录天数
+	ConditionTypeRechargeSumDay   = 10008 // 累计充值天数
+	ConditionTypeConsumeSum       = 10009 // 累计消费钻石数量
+)
+
 // 玩家活动数据结构体
 type UserEventSt struct {
 	EventId     uint64         // 活动ID
