f31b0ae62ad07cdd981aea944dd95ebe008e99fd
byteedu <1528884535@qq.com>
2020-10-04 18:56:46 +0800

    code

--- rewards/rewards.go
+++ rewards/rewards.go
@@ -1,13 +1,13 @@
-package  twlib_rewards
+package twlib_rewards
 
 // 奖励列表
 type RewardSt struct {
-	ItemId    int
-	ItemType  int
-	RewardNum int
+	ItemId   int
+	ItemType int
+	ItemNum  int
 }
 
 // 获取奖励列表
-func GetRewardInfo()  {
+func GetRewardInfo() {
 
 }
